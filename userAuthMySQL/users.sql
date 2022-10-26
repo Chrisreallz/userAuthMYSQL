e090763298b48e98278902cc137a442b1a8b809f
Chrisreallz <111513047+Chrisreallz@users.noreply.github.com>
2022-10-27 07:16:37 +0800

    Update users.sql

---
+++ userAuthMySQL/users.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Students` (`id`, `full_names`, `email`, `gender`, `country`, `password`) VALUES
+(2, 'Nancy Vicky', 'nancy@gmail.com', 'Female', 'Nigeria', 'andy'),
+(3, 'Seyi Olufe', 'seyi@gmail.com', 'Male', 'Nigeria', '1234'),
+(4, 'Chioma Victoria', 'vicky@gmail.com', 'Female', 'Cameroon', '129323'),
+(8, 'Nfon Andrew', 'drew@gmail.com', 'Nigeria', 'USA', 'tatah');
